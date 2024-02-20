525c0d9f7d90b8f0c357d2b93ade995a6d27a05d
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2024-02-20 18:20:21 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('unusedVariable is not used in the component', () => {
-  // Render the component
+test('renders learn react link', () => {
   render(<App />);
-
-  // Attempt to access the unusedVariable
-  // If the test fails, it means the variable is being used
-  expect(() => {
-    // Replace 'unusedVariable' with the actual name of the variable if needed
-    // eslint-disable-next-line no-unused-vars
-    const unusedVariable = (window as any).unusedVariable;
-  }).toThrow();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
