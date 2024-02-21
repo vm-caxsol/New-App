79c54b0e7dd50d04c7a7275b51b6d2bdd2d803f8
vm-caxsol <133189348+vm-caxsol@users.noreply.github.com>
2024-02-21 14:00:52 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/this text does not exist/i);  // intentionally incorrect text
+  expect(linkElement).toBeInTheDocument('Learn React link is missing');
 });
+
+test('renders the App logo', () => {
+  render(<App />);
+  const logoElement = screen.getByAltText(/logo/i);
+  expect(logoElement).toBeInTheDocument();
+});
+
+test('renders the code edit message', () => {
+  render(<App />);
+  const codeEditElement = screen.getByText(/edit .*src\/App\.js.* and save to reload\./i);
+  expect(codeEditElement).toBeInTheDocument();
+});
+
+
